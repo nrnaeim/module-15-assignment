47a92b58c917fcf225332a43af212eb60dd3f742
Niemur Rahman <nrnaeim@gmail.com>
2025-07-14 21:59:32 +0600

    Created router.js
    Express and controller module imported. router instance is created. get, post and all methods are created. all method is for 404 page not found. router is export as default

---
+++ src/routes/router.js
@@ -0,0 +1,25 @@
+/*
+ * Title: Router
+ * Description:
+ * Author: Niemur Rahman
+ * Email: nrnaeim@gmail.com
+ * Date: 14-07-2025
+ */
+
+//Dependencies
+//Core Dependencies
+//const  =require();
+
+//External Dependencies
+const express = require("express");
+
+//Local Dependencies
+const controller = require("../controllers/controller");
+
+const router = express.Router();
+
+router.get("/", controller.getRequest);
+router.post("/", controller.getRequest);
+router.all("*", controller.notFoundRequest);
+
+export default router;
